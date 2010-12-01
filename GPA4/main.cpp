8fb7317f51556acb6eb3d3d2330473d6a333b1d2
wahyu.g <wahyu.g@abf12c61-3e4e-1e1d-b3fc-2f84d3efec81>
2010-12-01 01:01:09 +0000

    testing

---
+++ GPA4/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: gbudiman
+ *
+ * Created on November 30, 2010, 7:59 PM
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
