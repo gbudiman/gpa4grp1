aa02ffe297aae7c095ad7e0c29701bbc985ff894
wahyu.g <wahyu.g@abf12c61-3e4e-1e1d-b3fc-2f84d3efec81>
2010-12-01 01:49:24 +0000

    About to start 2D view

---
+++ trunk/GPA4/moc_render2D.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'render2D.h'
+**
+** Created: Tue Nov 30 20:42:15 2010
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "render2D.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'render2D.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_render2D[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_render2D[] = {
+    "render2D\0"
+};
+
+const QMetaObject render2D::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_render2D,
+      qt_meta_data_render2D, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &render2D::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *render2D::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *render2D::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_render2D))
+        return static_cast<void*>(const_cast< render2D*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int render2D::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ trunk/GPA4/render2D.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   render2D.h
+ * Author: gbudiman
+ *
+ * Created on November 30, 2010, 8:31 PM
+ */
+
+#ifndef _RENDER2D_H
+#define	_RENDER2D_H
+#include <string>
+#include <QtGui>
+using namespace std;
+
+class render2D : public QWidget {
+    Q_OBJECT
+public:
+    render2D();
+    virtual ~render2D();
+    void setState(string state);
+protected:
+    void paintEvent(QPaintEvent* event);
+private:
+    string cubeState;
+};
+
+#endif	/* _RENDER2D_H */
+
