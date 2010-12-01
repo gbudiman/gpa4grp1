ed348afc69e34ea015452a07f2b2b9b5ee74e831
wahyu.g <wahyu.g@abf12c61-3e4e-1e1d-b3fc-2f84d3efec81>
2010-12-01 02:40:58 +0000

    2D done

--- trunk/GPA4/moc_render2D.cpp
+++ trunk/GPA4/moc_render2D.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'render2D.h'
 **
-** Created: Tue Nov 30 20:42:15 2010
+** Created: Tue Nov 30 21:33:35 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/GPA4/render2D.cpp
+++ trunk/GPA4/render2D.cpp
@@ -6,8 +6,11 @@
  */
 
 #include "render2D.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
-render2D::render2D() {
+render2D::render2D(QWidget* parent) : QWidget(parent) {
 }
 
 render2D::~render2D() {
@@ -18,30 +21,64 @@ void render2D::setState(string state) {
     update();
 }
 
-void render2D::paintEvent(QPaintEvent* event) {
+void render2D::paintEvent(QPaintEvent*) {
     QPainter painter(this);
     for(int i = 0; i < 9; i++) {
-        painter.setPen(Qt::SolidLine);
+        painter.setBrush(Qt::black);
         painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(100 + i%3 * 30 + 1, i/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
     }
     for(int i = 9; i < 18; i++) {
-        painter.setPen(Qt::SolidLine);
-        painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        painter.setBrush(Qt::black);
+        painter.drawRect(10 + (i-9)%3 * 30, i/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(10 + (i-9)%3 * 30 + 1, i/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
     }
     for(int i = 18; i < 27; i++) {
-        painter.setPen(Qt::SolidLine);
-        painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        painter.setBrush(Qt::black);
+        painter.drawRect(100 + (i-18)%3 * 30, (i-9)/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(100 + (i-18)%3 * 30 + 1, (i-9)/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
     }
     for(int i = 27; i < 36; i++) {
-        painter.setPen(Qt::SolidLine);
-        painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        painter.setBrush(Qt::black);
+        painter.drawRect(190 + (i-27)%3 * 30, (i-18)/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(190 + (i-27)%3 * 30 + 1, (i-18)/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
     }
     for(int i = 36; i < 45; i++) {
-        painter.setPen(Qt::SolidLine);
-        painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        painter.setBrush(Qt::black);
+        painter.drawRect(280 + (i-36)%3 * 30, (i-27)/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(280 + (i-36)%3 * 30 + 1, (i-27)/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
     }
     for(int i = 45; i < 54; i++) {
-        painter.setPen(Qt::SolidLine);
-        painter.drawRect(100 + i%3 * 30, i/3 * 30, 30, 30);
+        painter.setBrush(Qt::black);
+        painter.drawRect(100 + (i-45)%3 * 30, (i-27)/3 * 30, 30, 30);
+        //painter.setBrush(getColor(cubeState.at(i)));
+        painter.fillRect(100 + (i-45)%3 * 30 + 1, (i-27)/3 * 30 + 1, 30-1, 30-1, getColor(cubeState.at(i)));
+    }
+    painter.setRenderHint(QPainter::Antialiasing, true);
+}
+
+QBrush render2D::getColor(char s) {
+    if (s ==  'R') {
+        return Qt::red;
+    }
+    else if (s == 'Y') {
+        return Qt::yellow;
+    }
+    else if (s == 'B') {
+        return Qt::blue;
+    }
+    else if (s == 'O') {
+        return QBrush(QColor(255, 102, 0));
+    }
+    else if (s == 'W') {
+        return Qt::white;
+    }
+    else {
+        return QBrush(QColor(0, 255, 0));
     }
 }
